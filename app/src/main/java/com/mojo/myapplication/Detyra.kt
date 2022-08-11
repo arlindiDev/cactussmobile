f6ceaf9604fb4c68e47a911485149b2144e7eca0
ArditNimanaj <arditnimanaj@gmail.com>
2022-08-11 18:19:02 +0200

    a bug to fix

--- app/src/main/java/com/mojo/myapplication/Detyra.kt
+++ app/src/main/java/com/mojo/myapplication/Detyra.kt
@@ -10,6 +10,86 @@ import kotlin.random.Random
 
 
 class Detyra : AppCompatActivity() {
+    lateinit var emri: EditText
+    lateinit var mbiemri: EditText
+    lateinit var kafsha: EditText
+    lateinit var mosha: EditText
+
+    fun randomUsername1(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
+        if (kafsha.lowercase() == "dog") {
+            return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomDogName}")
+        }
+        if (kafsha.lowercase() == "dinosaur") {
+            return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomDinosaurName}")
+        }
+        if (kafsha.lowercase() == "cats") {
+            return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomCatName}")
+        } else {
+            return ("${emri.get(0)}.${mbiemri.get(0)}${mosha}")
+        }
+    }
+
+    fun randomUsername2(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
+        if (kafsha.lowercase() == "dog") {
+            return ("${emri}${mbiemri.get(0)}.${randomDogName}")
+        }
+        if (kafsha.lowercase() == "dinosaur") {
+            return ("${emri}${mbiemri.get(0)}.${randomDinosaurName}")
+        }
+        if (kafsha.lowercase() == "cats") {
+            return ("${emri}${mbiemri.get(0)}.${randomCatName}")
+        } else {
+            return ("${emri.take(2)}.${mbiemri.take(2)}${mosha}")
+        }
+    }
+
+    fun randomUsername3(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
+        if (kafsha.lowercase() == "dog") {
+            return ("${emri}${mbiemri}.${randomDogName}${mosha}")
+        }
+        if (kafsha.lowercase() == "dinosaur") {
+            return ("${emri}${mbiemri}.${randomDinosaurName}${mosha}")
+        }
+        if (kafsha.lowercase() == "cats") {
+            return ("${emri}${mbiemri}.${randomCatName}${mosha}")
+        } else {
+            return ("${emri.take(1)}.${mbiemri}${mosha}")
+        }
+    }
+
+
+    var randomUserNames = listOf(
+        randomUsername1(
+            emri.text.toString(),
+            mbiemri.text.toString(),
+            kafsha.text.toString(),
+            mosha.text
+        ), randomUsername2(
+            emri.text.toString(),
+            mbiemri.text.toString(),
+            kafsha.text.toString(),
+            mosha.text
+        ), randomUsername3(
+            emri.text.toString(),
+            mbiemri.text.toString(),
+            kafsha.text.toString(),
+            mosha.text
+        )
+
+    )
+    var randomDogName = listOf(
+        "Bubi",
+        "Pitbull",
+        "Husky",
+        "Dobbberman",
+        "Labrador",
+        "German Shepherd",
+        "Bulldog"
+    ).random()
+    var randomCatName =
+        listOf("Abyssinian", "Burmese", "British", "Cornish", "Devon", "Egyptian").random()
+    var randomDinosaurName =
+        listOf("Raptor", "Rex", "Robber", "Roof", "Stego", "Saurus", "Lizard").random()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,71 +104,7 @@ class Detyra : AppCompatActivity() {
         var tregoshkronjat = findViewById<TextView>(R.id.tregoShkronjat)
         var tregousernamin = findViewById<TextView>(R.id.tregousernamin)
 
-        var randomDogName = listOf("Bubi", "Pitbull", "Husky", "Dobbberman", "Labrador", "German Shepherd", "Bulldog").random()
-        var randomCatName = listOf("Abyssinian", "Burmese", "British", "Cornish", "Devon", "Egyptian").random()
-        var randomDinosaurName = listOf("Raptor", "Rex", "Robber", "Roof", "Stego", "Saurus", "Lizard").random()
 
-        lateinit var kafsha:String
-        fun randomUsername1(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
-            if (kafsha.lowercase() == "dog") {
-                return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomDogName}")
-            }
-            if (kafsha.lowercase() == "dinosaur") {
-                return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomDinosaurName}")
-            }
-            if (kafsha.lowercase() == "cats") {
-                return ("${emri.get(0)}${mbiemri.get(0)}${mosha}.${randomCatName}")
-            } else {
-                return ("${emri.get(0)}.${mbiemri.get(0)}${mosha}")
-            }
-        }
-
-        fun randomUsername2(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
-            if (kafsha.lowercase() == "dog") {
-                return ("${emri}${mbiemri.get(0)}.${randomDogName}")
-            }
-            if (kafsha.lowercase() == "dinosaur") {
-                return ("${emri}${mbiemri.get(0)}.${randomDinosaurName}")
-            }
-            if (kafsha.lowercase() == "cats") {
-                return ("${emri}${mbiemri.get(0)}.${randomCatName}")
-            } else {
-                return ("${emri.take(2)}.${mbiemri.take(2)}${mosha}")
-            }
-        }
-
-        fun randomUsername3(emri: String, mbiemri: String, kafsha: String, mosha: Editable): String {
-            if (kafsha.lowercase() == "dog") {
-                return ("${emri}${mbiemri}.${randomDogName}${mosha}")
-            }
-            if (kafsha.lowercase() == "dinosaur") {
-                return ("${emri}${mbiemri}.${randomDinosaurName}${mosha}")
-            }
-            if (kafsha.lowercase() == "cats") {
-                return ("${emri}${mbiemri}.${randomCatName}${mosha}")
-            } else {
-                return ("${emri.take(1)}.${mbiemri}${mosha}")
-            }
-        }
-
-
-        var randomUserNames = listOf(randomUsername1(
-                emri.text.toString(),
-                mbiemri.text.toString(),
-                kafsha ,
-                mosha.text
-            ), randomUsername2(
-                emri.text.toString(),
-                mbiemri.text.toString(),
-                kafsha ,
-                mosha.text
-            ), randomUsername3(
-                emri.text.toString(),
-                mbiemri.text.toString(),
-                kafsha,
-                mosha.text
-            )
-        )
         butoni.setOnClickListener {
             val gjatesiaeEmrit = emri.text.length
             var kafsha = animal.text
