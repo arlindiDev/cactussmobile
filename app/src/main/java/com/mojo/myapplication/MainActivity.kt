89d35f76b3e1d157fe5b53ed532cf0f670251b2d
HachiApisu <blete.isufi@outlook.com>
2022-08-13 09:21:28 +0200

    Jan shtuar butonat dhe teksti eshte fshehur

--- app/src/main/java/com/mojo/myapplication/MainActivity.kt
+++ app/src/main/java/com/mojo/myapplication/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.mojo.myapplication
 
+import android.annotation.SuppressLint
+import android.media.Image
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.LinearLayoutCompat
 
@@ -10,29 +13,84 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var content: LinearLayoutCompat
 
+
+    open class Animal(
+        val name: String,
+        @DrawableRes var image: Int,
+        val age: String,
+        val description: String,
+    )
+
+    class Dog(
+        name: String,
+        image: Int,
+        age: String,
+        description: String
+    ) : Animal(name, image, age, description)
+
+
+    class Cat(
+        name: String,
+        image: Int,
+        age: String,
+        description: String,
+        val isCute: Boolean
+    ) : Animal(name, image, age, description)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         content = findViewById(R.id.content)
 
-        val animals = mapOf(
-            "Bubi" to R.drawable.dog,
-            "Haski" to R.drawable.dog1,
-            "Sharri" to R.drawable.dog2,
-            "Reksi" to R.drawable.dog3,
-            "Dobermani" to R.drawable.dog4,
-            "Qeni" to R.drawable.dog5
+
+
+
+        val animals = listOf(
+            Dog("Bubi", R.drawable.dog, "Mosha: 6 muaj", "Bubi eshte nje Laborador Retriever, shume i embel."),
+            Dog("Reksi", R.drawable.dog1, "Mosha: 1 vjet", "Reksi eshte nje Kerry Beagle dhe i ka shume qef lojrat."),
+            Dog("Tokyo", R.drawable.dog2, "Mosha: 5 vjet", "Tokyo eshte nje Golden Retriever dhe i do te gjithe."),
+            Cat("Lara", R.drawable.cat, "Mosha: 3 muaj", "Lara eshte nje Munchkin. Eshte lozonjare dhe e dashur.", true)
         )
 
-        for(animal in animals) {
+
+        for (animal in animals) {
             val animalItem = layoutInflater.inflate(R.layout.animal_item, null)
 
             val animalNameText = animalItem.findViewById<TextView>(R.id.animalNameText)
-            val animalImage = animalItem.findViewById<ImageView>(R.id.animalImage)
+            val animalImage = animalItem.findViewById<ImageButton>(R.id.animalImage)
+            val descriptionText = animalItem.findViewById<TextView>(R.id.descriptionText)
+            val ageText = animalItem.findViewById<TextView>(R.id.ageText)
+            val cuteText = animalItem.findViewById<TextView>(R.id.isCute)
+            val likeButoni= animalItem.findViewById<ImageView>(R.id.likeButton)
+            val tekstiTeButoni = animalItem.findViewById<TextView>(R.id.tekstiTeButoni)
 
-            animalNameText.text = animal.key
-            animalImage.setImageResource(animal.value)
+                likeButoni.setOnClickListener(){
+                    if (likeButoni.isClickable){
+                      tekstiTeButoni.visibility = View.VISIBLE
+                    }
+                }
+
+            animalImage.setOnClickListener(){
+                if (animalImage.isClickable){
+                    ageText.visibility =View.VISIBLE
+                    descriptionText.visibility =View.VISIBLE
+                    animalNameText.visibility =View.VISIBLE
+                    if (animal is Cat && animal.isCute) {
+                        cuteText.visibility = View.VISIBLE
+                    }
+                }
+            }
+
+
+
+            animalNameText.text = animal.name
+            animalImage.setImageResource(animal.image)
+            descriptionText.text = animal.description
+            ageText.text = animal.age
+
+
 
             content.addView(animalItem)
         }
